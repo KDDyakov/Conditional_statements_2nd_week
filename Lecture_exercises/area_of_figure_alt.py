91afccc1bda721f773123dcb6401fc7d78c1ea5f
KDDyakov <konstantin.dyakov91@gmail.com>
2022-05-14 13:33:03 +0300

    Initial commit

---
+++ Lecture_exercises/area_of_figure_alt.py
@@ -0,0 +1,20 @@
+from math import pi
+
+figure = input()
+
+if figure == "square":
+    square_side = float(input())
+    S = square_side * square_side
+elif figure == "rectangle":
+    rectangle_side_first = float(input())
+    rectangle_side_second = float(input())
+    S = rectangle_side_first * rectangle_side_second
+elif figure == "circle":
+    circle_radius = float(input())
+    S = pi * (circle_radius * circle_radius)
+else:
+    figure == "triangle"
+    triangle_length = float(input())
+    triangle_height = float(input())
+    S = (triangle_height * triangle_length) / 2
+print(f'{S:.3f}')
---
+++ Lecture_exercises/greater_number.py
@@ -0,0 +1,6 @@
+number1 = input()
+number2 = input()
+if number1 >= number2:
+    print(number1)
+elif number2 >= number1:
+    print(number2)
